a4c4ae3a27cda926f0b80778d135957fa1d17f50
Alexey <avd5522@gmail.com>
2022-08-07 17:51:33 +0300

    deleted files

--- contact0.php
+++
@@ -1,9 +0,0 @@
-<div class="about"> 
-  <div>
-  
-  <span style="display: flex; justify-content: center; font-size: 16px;">
-   <p>По всем вопросам обращать на электронную почту по адресу: DobromyslovAV@mpei.ru</p>
-  </span>
-  </div>
-
-</div>
